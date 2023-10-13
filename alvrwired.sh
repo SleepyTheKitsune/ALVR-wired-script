837fa76dc83d96f60b32d7ee0e0c5437d0c2d9c0
Sleepy <108374023+SleepyTheKitsune@users.noreply.github.com>
2023-10-12 23:37:08 -0500

    Update alvrwired.sh

--- alvrwired.sh
+++ alvrwired.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 echo "Starting Script!"
-adb devices
+adb start-server
 sleep 1
 adb forward tcp:9943 tcp:9943
 adb forward tcp:9944 tcp:9944
 sleep 1
-echo "Headset Connected! have fun! :3"
+echo "VR Headset Connected!"
+echo "Have Fun!"
